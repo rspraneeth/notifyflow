426d6e5fad88b3856d320949264c9f618fdff34f
Satya Praneeth Reddi <rspraneeth.rsp@gmail.com>
2026-03-28 15:30:20 -0500

    feat: core processor setup & persisitent volumes setup for redis and postgres in docker

---
+++ core-processor/src/main/java/com/notifyflow/coreprocessor/CoreProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.notifyflow.coreprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreProcessorApplication.class, args);
+    }
+
+}
