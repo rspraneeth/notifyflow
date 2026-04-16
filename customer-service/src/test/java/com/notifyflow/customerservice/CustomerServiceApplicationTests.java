d4d2762b90e1dd8f9b2db18c65f334245e0868be
Satya Praneeth Reddi <rspraneeth.rsp@gmail.com>
2026-04-16 17:37:32 -0500

    feat: delivery-service complete, full end-to-end pipeline working

--- customer-service/src/test/java/com/notifyflow/customerservice/CustomerServiceApplicationTests.java
+++ customer-service/src/test/java/com/notifyflow/customerservice/CustomerServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.notifyflow.customerservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class CustomerServiceApplicationTests {
 
     @Test
--- payload-producer/src/test/java/com/notifyflow/payloadproducer/PayloadProducerApplicationTests.java
+++ payload-producer/src/test/java/com/notifyflow/payloadproducer/PayloadProducerApplicationTests.java
@@ -3,7 +3,7 @@ package com.notifyflow.payloadproducer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class PayloadProducerApplicationTests {
 
     @Test
